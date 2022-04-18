70e5d9545f75c5c343af96081d4deb104e4cf154
viajeslepotaxie7 <103970486+viajeslepotaxie7@users.noreply.github.com>
2022-04-18 18:44:33 -0500

    Initial commit

---
+++ code.js
@@ -0,0 +1,17 @@
+console.log("Fuerx fife 🥑👄🥑");
+
+function boletx() {
+
+	var namx = document.getElementById('nombrx').value;
+	document.getElementById("nxmbrx").innerHTML = namx;
+
+	var citx = document.getElementById('ciudxd').value;
+	document.getElementById("cxxdxd").innerHTML = citx;
+
+	var id = Math.floor(Math.random() * 999999);
+	document.getElementById("xd").innerHTML = id;
+
+	var costx = Math.floor(Math.random() * 150);
+	document.getElementById("vxxlx").innerHTML = costx;
+
+}
